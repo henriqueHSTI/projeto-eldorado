78c1dc4286df2a77476dc98a5a33898c75a28147
José Silva <henrique@hsti.eng.br>
2023-01-31 01:07:06 -0300

    Commit inicial

---
+++ ms-schedule/src/main/java/com/eldorado/microservico/schedule/controller/ScheduleController.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservico.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedule")
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduleController {
+}
